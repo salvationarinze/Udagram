966f5312dafa6bbbdb4a0e636d30306453a62872
Salvation Arinze <modernbabbage@gmail.com>
2021-05-12 02:16:40 +0100

    Added screenshots

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3e81517b2c48409bb09a49b15e65b37-283318588.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
